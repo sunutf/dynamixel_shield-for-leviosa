500ac43f0c5b46f798d61384c0e081ec6ea8d1b6
DESKTOP-9U03OGO\선종현 <123>
2019-04-27 21:02:45 +0900

    change array for constant value

--- leviosa_testbed_auto/leviosa_testbed_auto_esp32_eeprom/leviosa_testbed_auto_eeprom/leviosa_testbed_auto_eeprom.ino
+++ leviosa_testbed_auto/leviosa_testbed_auto_esp32_eeprom/leviosa_testbed_auto_eeprom/leviosa_testbed_auto_eeprom.ino
@@ -1,13 +1,18 @@
 #include "EEPROM.h"
 //소숫점 2자리까지 저장.
+#define QUAD_CNT 4
+#define HEIGHT_CNT 3
+#define DEG_CNT 2
 int addr = 0;
 
-float offset_val[4][3][4] = {
-  {{12.1234,0.5,0.6,0.4},{0.1,0.2,0.3,0.4},{0,0,0,0},},
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},},
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},},
-  {{0,0,0,0},{0,0,0,0},{0,0,0,0},}
+float offset_val[4][3][2] = {
+  {{12.1234,0.5},{0.1,0.2},{0,0},},
+  {{0,0},{0,0},{0,0},},
+  {{0,0},{0,0},{0,0},},
+  {{0,0},{0,0},{0,0},}
 };
+
+
 #define EEPROM_SIZE 128
 void setup() {
   // put your setup code here, to run once:
@@ -18,9 +23,9 @@ void setup() {
     Serial.println("failed to initialise EEPROM"); delay(1000000);
   }
   Serial.println("===READ===");
-  for(int quad = 0; quad<4; quad++){
-   for(int height = 0; height<3; height++){
-      for(int deg = 0; deg<4; deg++){
+  for(int quad = 0; quad<QUAD_CNT; quad++){
+   for(int height = 0; height<HEIGHT_CNT; height++){
+      for(int deg = 0; deg<DEG_CNT; deg++){
            addr +=4;
            Serial.println(EEPROM.readFloat(addr), 4);
        }
@@ -28,9 +33,9 @@ void setup() {
   }
   addr = 0;
   Serial.println("===WRITE===");
-  for(int quad = 0; quad<4; quad++){
-    for(int height = 0; height<3; height++){
-       for(int deg = 0; deg<4; deg++){
+  for(int quad = 0; quad<QUAD_CNT; quad++){
+    for(int height = 0; height<HEIGHT_CNT; height++){
+       for(int deg = 0; deg<DEG_CNT; deg++){
            addr +=4;
            EEPROM.writeFloat(addr, offset_val[quad][height][deg]);
            Serial.print(offset_val[quad][height][deg],4); Serial.println(" ");
